6ddfb2842933e4e96b6c9e23d60a58f3c0004a3e
Alice2222 <623387518@qq.com>
2017-06-13 18:23:56 +0800

    test

---
+++ config/default.js
@@ -0,0 +1,13 @@
+module.exports = {
+	port: 8001,
+	url: 'mongodb://localhost:27017/myapp',
+	session: {
+		name: 'SID',
+		secret: 'SID',
+		cookie: {
+			httpOnly: true,
+		    secure:   false,
+		    maxAge:   365 * 24 * 60 * 60 * 1000,
+		}
+	}
+}
---
+++ mongodb/db.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+		config = require('config');
